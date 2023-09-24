893ed3386e7fe013a6cf4f5557b900dab5babab8
Evgeniy Kukarskiy <jon.79555@gmail.com>
2023-09-24 22:03:26 +0700

    homework done

---
+++ main.py
@@ -0,0 +1,10 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+from datetime import datetime
+from application.app import app
+
+if __name__ == '__main__':
+    print(datetime.now())
+    get_employees()
+    calculate_salary()
+    app.run()
